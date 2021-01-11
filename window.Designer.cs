db1a8f5f9f408ab027eddd27ec7e24dda6c61305
hafbakh <77297289+hafbakh@users.noreply.github.com>
2021-01-11 20:13:01 +0100

    Add files via upload

---
+++ window.Designer.cs
@@ -0,0 +1,224 @@
+﻿
+namespace TP1
+{
+    partial class Ndelappli
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Ndelappli));
+            this.lbl_nom = new System.Windows.Forms.Label();
+            this.Lbl_Prenom = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.Lbl_Sexe = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.Linklbl_twitter = new System.Windows.Forms.LinkLabel();
+            this.btn_afficher = new System.Windows.Forms.Button();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbl_nom
+            // 
+            this.lbl_nom.AutoSize = true;
+            this.lbl_nom.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.lbl_nom.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_nom.Location = new System.Drawing.Point(139, 93);
+            this.lbl_nom.Name = "lbl_nom";
+            this.lbl_nom.Size = new System.Drawing.Size(54, 24);
+            this.lbl_nom.TabIndex = 1;
+            this.lbl_nom.Text = "Nom";
+            this.lbl_nom.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Lbl_Prenom
+            // 
+            this.Lbl_Prenom.AutoSize = true;
+            this.Lbl_Prenom.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.Lbl_Prenom.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lbl_Prenom.Location = new System.Drawing.Point(139, 135);
+            this.Lbl_Prenom.Name = "Lbl_Prenom";
+            this.Lbl_Prenom.Size = new System.Drawing.Size(83, 24);
+            this.Lbl_Prenom.TabIndex = 3;
+            this.Lbl_Prenom.Text = "Prénom";
+            this.Lbl_Prenom.Click += new System.EventHandler(this.label2_Click_1);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Homme",
+            "Femme",
+            "Non spécifié"});
+            this.comboBox1.Location = new System.Drawing.Point(241, 172);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(173, 21);
+            this.comboBox1.TabIndex = 4;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(241, 98);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(173, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(241, 135);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(173, 20);
+            this.textBox2.TabIndex = 6;
+            // 
+            // Lbl_Sexe
+            // 
+            this.Lbl_Sexe.AutoSize = true;
+            this.Lbl_Sexe.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.Lbl_Sexe.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lbl_Sexe.Location = new System.Drawing.Point(139, 172);
+            this.Lbl_Sexe.Name = "Lbl_Sexe";
+            this.Lbl_Sexe.Size = new System.Drawing.Size(58, 24);
+            this.Lbl_Sexe.TabIndex = 8;
+            this.Lbl_Sexe.Text = "Sexe";
+            this.Lbl_Sexe.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(139, 217);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 24);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Adresse";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(241, 217);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(220, 20);
+            this.textBox3.TabIndex = 10;
+            // 
+            // Linklbl_twitter
+            // 
+            this.Linklbl_twitter.AutoSize = true;
+            this.Linklbl_twitter.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Linklbl_twitter.Location = new System.Drawing.Point(723, 419);
+            this.Linklbl_twitter.Name = "Linklbl_twitter";
+            this.Linklbl_twitter.Size = new System.Drawing.Size(65, 22);
+            this.Linklbl_twitter.TabIndex = 11;
+            this.Linklbl_twitter.TabStop = true;
+            this.Linklbl_twitter.Text = "Twitter";
+            this.Linklbl_twitter.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // btn_afficher
+            // 
+            this.btn_afficher.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.btn_afficher.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btn_afficher.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_afficher.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.btn_afficher.Location = new System.Drawing.Point(332, 288);
+            this.btn_afficher.Name = "btn_afficher";
+            this.btn_afficher.Size = new System.Drawing.Size(248, 39);
+            this.btn_afficher.TabIndex = 12;
+            this.btn_afficher.Text = "Afficher";
+            this.btn_afficher.UseVisualStyleBackColor = false;
+            this.btn_afficher.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.linkLabel1.DisabledLinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.linkLabel1.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkLabel1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.linkLabel1.Location = new System.Drawing.Point(699, 356);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(69, 22);
+            this.linkLabel1.TabIndex = 15;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Twitter";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked_1);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 354);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(180, 18);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "Développé par hafbakh";
+            this.label2.Click += new System.EventHandler(this.label2_Click_2);
+            // 
+            // Ndelappli
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(770, 381);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.btn_afficher);
+            this.Controls.Add(this.Linklbl_twitter);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Lbl_Sexe);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.Lbl_Prenom);
+            this.Controls.Add(this.lbl_nom);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Ndelappli";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Contact";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lbl_nom;
+        private System.Windows.Forms.Label Lbl_Prenom;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label Lbl_Sexe;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.LinkLabel Linklbl_twitter;
+        private System.Windows.Forms.Button btn_afficher;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ window.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TP1
+{
+    public partial class Ndelappli : Form
+    {
+        public Ndelappli()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start("https://twitter.com/Hafbakh");
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void linkLabel1_LinkClicked_1(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start("https://www.twitter.com/hafbakh");
+        }
+
+        private void label2_Click_2(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
